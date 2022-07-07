80cc2e029b7eb921e2d34ae98cb384503c5935d0
Koryto Daniil <DKoryto@alfabank.ru>
2022-07-08 00:16:27 +1000

    added EnableDiscoveryClient

--- src/main/kotlin/ru/dankos/api/stockservice/StockServiceApplication.kt
+++ src/main/kotlin/ru/dankos/api/stockservice/StockServiceApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 import ru.dankos.api.stockservice.config.CaffeineProperties
 import ru.dankos.api.stockservice.config.TinkoffProperties
@@ -11,6 +12,7 @@ import ru.dankos.api.stockservice.config.TinkoffProperties
 
 @EnableCaching
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(
     TinkoffProperties::class, CaffeineProperties::class
