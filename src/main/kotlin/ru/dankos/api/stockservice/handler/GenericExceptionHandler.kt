bd36b0931ed0ddf91b3a94e9139b5e994f3c2c5e
Koryto Daniil <DKoryto@alfabank.ru>
2022-08-12 03:17:24 +1000

    deleted exception handling

--- src/main/kotlin/ru/dankos/api/stockservice/handler/GenericExceptionHandler.kt
+++ src/main/kotlin/ru/dankos/api/stockservice/handler/GenericExceptionHandler.kt
@@ -2,26 +2,21 @@ package ru.dankos.api.stockservice.handler
 
 import mu.KLogging
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
-import org.springframework.http.HttpStatus.NOT_FOUND
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import ru.dankos.api.stockservice.exception.StockNotFoundException
 import java.util.*
 
 @RestControllerAdvice
 class GenericExceptionHandler {
 
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(value = [Exception::class])
-    internal fun handleAnyException(exception: Exception): ErrorResponse =
-        exception.toErrorResponse(INTERNAL_SERVER_ERROR).apply { logger.error { exception } }
-
-    @ResponseStatus(NOT_FOUND)
-    @ExceptionHandler(value = [StockNotFoundException::class])
-    internal fun handleEntityNotFoundException(exception: StockNotFoundException): ErrorResponse =
-        exception.toErrorResponse(NOT_FOUND).apply { logger.error { exception } }
+//    @ResponseStatus(INTERNAL_SERVER_ERROR)
+//    @ExceptionHandler(value = [Exception::class])
+//    internal fun handleAnyException(exception: Exception): ErrorResponse =
+//        exception.toErrorResponse(INTERNAL_SERVER_ERROR).apply { logger.error { exception } }
+//
+//    @ResponseStatus(NOT_FOUND)
+//    @ExceptionHandler(value = [StockNotFoundException::class])
+//    internal fun handleEntityNotFoundException(exception: StockNotFoundException): ErrorResponse =
+//        exception.toErrorResponse(NOT_FOUND).apply { logger.error { exception } }
 
     private fun Exception.toErrorResponse(httpStatus: HttpStatus): ErrorResponse = ErrorResponse(
         message = this.message,
